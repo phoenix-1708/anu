0616f3a444c0fbe12951ef96050679b8641f8dad
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2022-11-30 16:20:11 +0530

    Update final.sh

--- final.sh
+++ final.sh
@@ -4,10 +4,13 @@ cd /tmp/rom
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 20G
+ccache -M 12G
 ccache -o compression=true
 ccache -z
 
 . build/envsetup.sh
 $lunch
+export BUILD_USERNAME=phoenix1708
+export BUILD_HOSTNAME=prototype
+export TZ=Asia/Jakarta
 $make -j8
